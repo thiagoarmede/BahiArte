255d0a33658ed95c9a03b575acd7be2a0f33b046
Thiago Armede <thiago.armede@cubos.io>
2018-05-18 10:40:24 -0300

    innitial header

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 
 import React from 'react';
-import { View, Text } from 'react-native';
-import { AppLoading, Font } from 'expo';
-
-import FontAwesome  
+import { View, Text, StyleSheet, Platform, StatusBar } from 'react-native';
+import { AppLoading, Font, Expo } from 'expo';
+import FontAwesome    
 from './node_modules/@expo/vector-icons/fonts/FontAwesome.ttf';
 import MaterialIcons  
 from './node_modules/@expo/vector-icons/fonts/MaterialIcons.ttf';
+import {} from 'native-base';
+import { Container, Header, Left, Body, Right, Button, Icon, Title } from 'native-base';
+
+styles = StyleSheet.create({
+	headerStyle: {
+		backgroundColor: "#002141",
+	} 
+});
 
 export default class App extends React.Component {
 
@@ -18,7 +25,10 @@ export default class App extends React.Component {
     try {
       await Font.loadAsync({
         FontAwesome,
-        MaterialIcons
+		MaterialIcons,
+		Roboto: require("native-base/Fonts/Roboto.ttf"),
+		Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+		Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
       });
       this.setState({ fontLoaded: true });
     } catch (error) {
@@ -27,13 +37,29 @@ export default class App extends React.Component {
   }
 
 
+
   render() {
     if (!this.state.fontLoaded) {
       return <AppLoading />;
     }
 
     return (
-      <View>
+      <View style={{paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight}}>
+	  	<StatusBar
+			backgroundColor="#002141"  
+		/>
+        <Container>
+         	<Header style={styles.headerStyle}>
+				<Left>
+					<Button transparent>
+						<Icon name='menu'/>
+					</Button>
+				</Left>
+				<Body>
+					<Title>BahiArte</Title>
+				</Body>
+          	</Header>
+        </Container>
         <Text>My App</Text>
       </View>
     );
