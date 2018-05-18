d9e25394093a92b9e342ee0b22ef3e0f814af441
Diego Santos <diohmalta@gmail.com>
2018-05-18 11:28:30 -0300

    Create Button for index bottom

---
+++ src/components/buttonIndexBottom.js
@@ -0,0 +1,18 @@
+import React, {
+    Component
+} from 'react';
+
+import {
+    View,
+    TouchableNativeFeedback
+} from 'react-native';
+
+export default class Button extends Component {
+    render() {
+        return ( <
+            View >
+            <
+            /View>
+        )
+    }
+}
